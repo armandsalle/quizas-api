e13c92bb6b36f5c8e26aa5cb867384b7d59d8bde
armand <armand.salle@gmail.com>
2022-03-07 00:31:19 +0100

    typo

--- lib/googleSheets.ts
+++ lib/googleSheets.ts
@@ -21,7 +21,7 @@ export async function addBookingRequestToGoogleSheet(
       requestBody: {
         values: [
           [
-            "NON",
+            "N",
             data.name,
             data.lastname,
             data.email,
